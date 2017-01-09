aa1444e8570aaba645690983d8f63ff8e1064dd5
muriel <murielthoms@gmail.com>
2017-01-09 11:28:08 +0100

    exercise 3 fini

--- loop/exercise2.php
+++ loop/exercise2.php
@@ -4,7 +4,6 @@ $m = rand(1, 100);
 for ($i=0; $i <20 ; $i++) { 
 	$add = $i * $m ."\n";
 	echo $add;
-	
 }
 
 ?>
---
+++ loop/exercise3.php
@@ -0,0 +1,11 @@
+<?php
+
+$deux = 12;
+
+for ($une=100; $une >=10 ; $une--) { 
+	$mult = $une * $deux;
+	echo $mult ."\n";
+	
+}
+
+?>
