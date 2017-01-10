48d24fdf87d71a35ab5a31b3537b84d613fb3a43
muriel <murielthoms@gmail.com>
2017-01-10 11:52:46 +0100

    exercise 4 fait

---
+++ tableaux/exercise4t.php
@@ -0,0 +1,21 @@
+<?php
+
+$mois = [
+
+"Janvier",
+"Février",
+"Mars",
+"Avril",
+"Mai",
+"Juin",
+"Juillet",
+"Aout",
+"Septembre",
+"Octobre",
+"Novembre",
+"Décembre"
+
+	];
+ $mois[7]="août";
+ var_dump($mois);
+?>
