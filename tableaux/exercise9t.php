4ec176e27a55eaa6aacb5764f7904a109b4b324d
muriel <murielthoms@gmail.com>
2017-01-10 14:39:34 +0100

    exercise 9 fait

---
+++ tableaux/exercise9t.php
@@ -0,0 +1,15 @@
+<?php
+$hautsFrance = array(
+	"01"=>"Aisne",
+	"59"=>"Nord",
+	"60"=>"Oise",
+	"62"=>"Pas de Calais",
+	"80"=>"Somme"
+	);
+foreach ($hautsFrance as $key => $value) {
+	echo"<ul>$value</ul>";
+
+}
+
+
+?>
