618b021e111fd3d3886ac49bf0c7c85aa6cf9483
muriel <murielthoms@gmail.com>
2017-01-10 11:31:59 +0100

    exercise 3 fait

---
+++ tableaux/exercise2t.php
@@ -0,0 +1,21 @@
+<?php
+
+$mois = [
+
+"Janvier",
+"Février",
+"Mars",
+"Avril",
+"Mai",
+"Juin",
+"Juillet",
+"Aout",
+"Septembre",
+"Octobre",
+"Novembre",
+"Décembre"
+
+	];
+echo $mois[2];
+
+?>
