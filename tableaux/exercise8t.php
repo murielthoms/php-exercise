43888bd02b1e53bff63112a0a814162204d227e4
muriel <murielthoms@gmail.com>
2017-01-10 14:32:31 +0100

    exercise 8 fait

---
+++ tableaux/exercise8t.php
@@ -0,0 +1,24 @@
+<?php 
+
+$mois = [
+
+"Janvier",
+"Février",
+"Mars",
+"Avril",
+"Mai",
+"Juin",
+"Juillet",
+"Aout",
+"Septembre",
+"Octobre",
+"Novembre",
+"Décembre"
+
+];
+
+foreach ($mois as $key => $value) {
+	echo "<ul>$value</ul>";
+}
+
+?>
