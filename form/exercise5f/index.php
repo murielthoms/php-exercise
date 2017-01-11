e31470b82ce747b8f69556a6d23b7a17169a8547
muriel <murielthoms@gmail.com>
2017-01-11 12:26:30 +0100

    exercise 5 fait

---
+++ form/exercise5f/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form method="GET" action="">
+		<select name="genre">
+
+			<option value="Mme">Madame</option>
+			<option value="Mlle">Mademoiselle</option>
+			<option value="Mr">Monsieur</option>
+
+		</select>
+
+		<div>
+			<label for="name">Nom</label>
+			<input type="text" name="nom" placeholder="Entrez votre nom ...">
+		</div>
+		<div>
+			<label for="prenom">Prénom</label>
+			<input type="text" name="prenom" placeholder="Entrez votre prénom ...">
+		</div>
+		<div>
+			<button type="submit" name="valider">Validez</button>
+		</div>
+
+	</form>
+	
+	
+</body>
+</html>
