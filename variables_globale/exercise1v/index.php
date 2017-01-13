b2e1afe8879b34ad47b3df9f5718691aa78a5f95
muriel <murielthoms@gmail.com>
2017-01-13 09:58:02 +0100

    exercise 1 fait

---
+++ variables_globale/exercise1v/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<?php
+
+echo $_SERVER['HTTP_USER_AGENT'];
+echo $_SERVER['REMOTE_ADDR'];
+echo $_SERVER['SERVER_NAME'];
+
+?>
+	
+</body>
+</html>
